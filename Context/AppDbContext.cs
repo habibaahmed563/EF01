319b2fc334d63ef89b04bb8b9dfe4993a9f1e2d0
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-31 22:38:20 +0300

    demo

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -14,6 +14,48 @@ namespace EF01.Context
         //Microsoft.EntityframeworkCore
 
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //    modelBuilder.Entity<Employee>().HasKey("EmpId");
+            ////    modelBuilder.Entity<Employee>().HasKey(E=>E.EmpId);
+            //modelBuilder.Entity<Employee>()
+            //    .Property(E => E.Name)
+            //    .IsRequired()
+            //    .HasColumnType("nvarchar")
+            //    .HasMaxLength(50)
+            //    .HasColumnName("EmployeeName");
+
+            //modelBuilder.Entity<Employee>().Property(E => E.Age).IsRequired(false);
+
+            //modelBuilder.Entity<Employee>().Property(E => E.Salary).HasColumnType("Money");
+
+            //modelBuilder.Entity<Employee>().Property(E => E.DateOfCreation).HasDefaultValue(DateTime.Now);
+
+            //modelBuilder.Entity<Employee>().Property(E => E.DateOfCreation).HasDefaultValueSql("GetDate()");
+
+
+
+            modelBuilder.Entity<Employee>(E =>
+            { 
+                E
+                .Property(E => E.Name)
+                .IsRequired()
+                .HasColumnType("nvarchar")
+                .HasMaxLength(50)
+                .HasColumnName("EmployeeName");
+
+                E.Property(E => E.Age).IsRequired(false);
+
+                E.Property(E => E.Salary).HasColumnType("Money");
+
+                E.Property(E => E.DateOfCreation).HasDefaultValue(DateTime.Now);
+
+                E.Property(E => E.DateOfCreation).HasDefaultValueSql("GetDate()");
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server = .; Database = AppG03 ; Trusted_Connection = True");
